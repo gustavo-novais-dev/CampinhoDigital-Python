f6bd8e5b4e6d2a3a9ab08e56b8831a321b60d3fc
gustavo-novais-dev <gustavonovais.dev@gmail.com>
2024-03-20 00:23:28 +0000

    [FEAT] numeric types

--- lab-02.py
+++ lab-02.py
@@ -1,9 +1,33 @@
 print("Python has three numeric types: int, float, and complex")
 
+
+#Criação de uma variável 
+
 myValue = 1
 
 print(myValue)
 
 print(type(myValue))
 
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+
+#Exercício 3: apresentação do tipo de dado float
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+
+#Exercício 4: apresentação do tipo de dado complex
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+
+#Exercício 5: apresentação do tipo de dado bool
+myValue=True
+print(myValue)
+print(type(myValue))
 print(str(myValue) + " is of the data type " + str(type(myValue)))
