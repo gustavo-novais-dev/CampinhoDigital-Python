94f55469339051982875a675a852350b5bdeab09
gustavo-novais-dev <gustavonovais.dev@gmail.com>
2024-03-20 22:41:59 +0000

    [FEAT] userReply = input("Would you like to buy stamps, buy an envelope, or make a copy? (Enter stamps, envelope, or copy) ")

--- lab-06/lab-06.py
+++ lab-06/lab-06.py
@@ -19,7 +19,7 @@ for key, value in myVehicle.items():
 
 myInventoryList = []
 
-with open('car_fleet.csv') as csvFile:
+with open('/home/ec2-user/environment/CampinhoDigital-Python/lab-06/car_fleet.csv') as csvFile:
     csvReader = csv.reader(csvFile, delimiter=',')  
     lineCount = 0  
     for row in csvReader:
---
+++ lab-07.py
@@ -0,0 +1,6 @@
+#Exercício 1: uso da declaração if
+
+userReply = input("Do you need to ship a package? (Enter yes or no) ")
+
+if userReply == "yes":
+    print("We can help you ship that package!")
