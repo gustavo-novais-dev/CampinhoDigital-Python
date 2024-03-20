7f3a34f9bad08f9220519610f75acb3dbd0157ae
gustavo-novais-dev <gustavonovais.dev@gmail.com>
2024-03-20 00:34:32 +0000

    [FEAT] list

---
+++ lab-04.py
@@ -0,0 +1,31 @@
+#Definição de uma lista
+
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+
+#Acesso a uma lista pela posição
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+
+#Alteração dos valores da lista
+myFruitList[2] = "orange"
+print(myFruitList)
+
+
+#Exercício 2: apresentação do tipo de dado tupla
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+
+#Acesso a um dicionário pelo nome
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
